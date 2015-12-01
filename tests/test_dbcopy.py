9fc7cfb4e416184297ec0f0806d88af67d658075
resteve <resteve@zikzakmedia.com>
2015-12-01 11:06:55 +0100

    Tests

--- tests/test_dbcopy.py
+++ tests/test_dbcopy.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part dbcopy module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the dbcopy module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class DbcopyTestCase(unittest.TestCase):
-    'Test DB Copy module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('dbcopy')
-
-    def test0005views(self):
-        'Test views'
-        test_view('dbcopy')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class DbcopyTestCase(ModuleTestCase):
+    'Test Dbcopy module'
+    module = 'dbcopy'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         DbcopyTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
