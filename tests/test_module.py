670f86fb43615cc5c605a6df2bb407d539b3887f
Albert Cervera i Areny <albert@nan-tic.com>
2022-05-03 18:36:34 +0200

    Remove CompanyTestMixin.

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,12 +1,9 @@
-
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class DbcopyTestCase(CompanyTestMixin, ModuleTestCase):
+class DbcopyTestCase(ModuleTestCase):
     'Test Dbcopy module'
     module = 'dbcopy'
 
